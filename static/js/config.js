ccb92521a1905632b09c7f7491b86719b3d8171f
Kellye Rennell <jkrennell@gmail.com>
2019-11-28 18:45:33 -0600

    file debugging

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-// API key
-=======
-// API key
->>>>>>> 316208f8e27326415af9a58c87021a8d6be15d8c
+//API Key 
 const API_KEY = "pk.eyJ1Ijoia3Jlbm5lbGwiLCJhIjoiY2sycnJyb29rMGQyeTNubW5kaWZ4NjV4eSJ9.cfRYG1Ny3cyl4IX-U4IMtw";
