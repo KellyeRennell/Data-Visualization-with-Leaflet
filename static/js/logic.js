76444b8c1cfe48436e71e52c69033b7aac5ce627
Kellye Rennell <jkrennell@gmail.com>
2019-11-28 18:43:09 -0600

    file debugging

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
- /*Import & Visualize the Data**
-
- Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
-
- * Your data markers should reflect the magnitude of the earthquake in their size and color. Earthquakes with higher magnitudes should appear larger and darker in color.
-
- * Include popups that provide additional information about the earthquake when a marker is clicked.
-
- * Create a legend that will provide context for your map data.
-
- * Your visualization should look something like the map above.*/
-
  // Store our API endpoint inside usgsUrl for USGS earthquake data for all earth quakes in the past 7 days
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
  // console.log(url)
